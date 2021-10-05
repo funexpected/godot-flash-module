6b3904628e0f9d7b699b7c3a7e8fb7359c3b02b1
Yakov Borevich <j.borevich@gmail.com>
2021-10-05 19:15:20 +0300

    move AnimationState-specific nodes to own namespace

--- register_types.cpp
+++ register_types.cpp
@@ -234,41 +234,6 @@ static void _editor_init() {
 	Ref<EditorExportFlash> flash_export;
 	flash_export.instance();
 	EditorExport::get_singleton()->add_export_plugin(flash_export);
-
-
-	// add custom themes for state machine nodes
-#define ADD_FRAME_STYLE(name, r, g, b)																				\
-	{																												\
-		Ref<Theme> theme = EditorNode::get_singleton()->get_editor_theme();											\
-		Ref<StyleBoxFlat> default_frame = theme->get_stylebox("state_machine_frame", "GraphNode");					\
-		Ref<StyleBoxFlat> default_selected_frame = theme->get_stylebox("state_machine_selectedframe", "GraphNode");	\
-		if (default_frame.is_valid() && default_selected_frame.is_valid()) {										\
-			Ref<StyleBoxFlat> new_frame = default_frame->duplicate(true);											\
-			Ref<StyleBoxFlat> new_selected_frame = default_selected_frame->duplicate(true);							\
-			new_frame->set_bg_color(Color(r, g, b, 0.7));															\
-			new_selected_frame->set_bg_color(Color(r, g, b, 0.9));													\
-			theme->set_stylebox("state_machine_frame", String(#name), new_frame);									\
-			theme->set_stylebox("state_machine_selectedframe", String(#name), new_selected_frame);					\
-		}																											\
-	};																												\
-
-	// monokai colors from https://gist.github.com/r-malon/8fc669332215c8028697a0bbfbfbb32a
-	// grey (empty)
-	ADD_FRAME_STYLE(AnimationNodeEmpty, 46.0/255.0, 46.0/255.0, 46.0/255.0);
-	
-	// green (animations)
-	ADD_FRAME_STYLE(AnimationNodeAnimation, 180.0/255.0, 210.0/255.0, 115.0/255.0);
-	ADD_FRAME_STYLE(AnimationNodeFlashClip, 180.0/255.0, 210.0/255.0, 115.0/255.0);
-	ADD_FRAME_STYLE(AnimationNodeSpineAnimation, 180.0/255.0, 210.0/255.0, 115.0/255.0);
-
-	// blue (complex animations)
-	ADD_FRAME_STYLE(AnimationNodeStateMachine, 108.0/255.0, 153.0/255.0, 187.0/255.0);
-
-	// orange (instants)
-	ADD_FRAME_STYLE(AnimationNodeStateUpdate, 229.0/255.0, 181.0/255.0, 103.0/255.0);
-
-	// purple (custom non-instants)
-	ADD_FRAME_STYLE(AnimationNodeDelay, 158.0/255.0, 134.0/255.0, 200.0/255.0);
 }
 #endif
 
@@ -276,12 +241,12 @@ static void _editor_init() {
 Ref<ResourceFormatLoaderFlashTexture> resource_loader_flash_texture;
 
 void register_flash_types() {
+	// core flash classes
 	ClassDB::register_class<FlashPlayer>();
 	ClassDB::register_class<FlashMachine>();
-	ClassDB::register_class<AnimationNodeEmpty>();
-	ClassDB::register_class<AnimationNodeDelay>();
 	ClassDB::register_class<AnimationNodeFlashClip>();
-	ClassDB::register_class<AnimationNodeStateUpdate>();
+
+	// resources
 	ClassDB::register_virtual_class<FlashElement>();
 	ClassDB::register_class<FlashTextureRect>();
 	ClassDB::register_class<FlashDocument>();
@@ -296,11 +261,12 @@ void register_flash_types() {
 	ClassDB::register_class<FlashBitmapInstance>();
 	ClassDB::register_class<FlashTween>();
 
+
+	// loader
 	resource_loader_flash_texture.instance();
 	ResourceLoader::add_resource_format_loader(resource_loader_flash_texture);
 
 #ifdef TOOLS_ENABLED
-
 	EditorNode::add_init_callback(_editor_init);
 #endif
 }
