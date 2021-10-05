63739b2c355c5f89abd5ca15ecb70257b00a7a33
Yakov Borevich <j.borevich@gmail.com>
2021-10-05 19:15:20 +0300

    move AnimationState-specific nodes to own namespace

--- animation_node_flash.h
+++ animation_node_flash.h
@@ -49,33 +49,6 @@ public:
 	FlashMachine();
 };
 
-class AnimationNodeEmpty: public AnimationRootNode {
-	GDCLASS(AnimationNodeEmpty, AnimationRootNode);
-public:
-	virtual float process(float p_time, bool p_seek);
-};
-
-class AnimationNodeDelay: public AnimationRootNode {
-	GDCLASS(AnimationNodeDelay, AnimationRootNode);
-
-	float min_delay;
-	float max_delay;
-	StringName time;
-	StringName delay;
-
-protected:
-	static void _bind_methods();
-public:
-	void get_parameter_list(List<PropertyInfo> *r_list) const;
-	void set_min_delay(float p_delay);
-	float get_min_delay() const;
-	void set_max_delay(float p_delay);
-	float get_max_delay() const;
-	virtual float process(float p_time, bool p_seek);
-
-	AnimationNodeDelay();
-
-};
 
 class AnimationNodeFlashClip: public AnimationRootNode {
     GDCLASS(AnimationNodeFlashClip, AnimationRootNode);
@@ -106,32 +79,6 @@ public:
 
 	AnimationNodeFlashClip();
 };
-
-class AnimationNodeStateUpdate: public AnimationRootNode {
-    GDCLASS(AnimationNodeStateUpdate, AnimationRootNode);
-
-	StringName time;
-	Dictionary state_update;
-
-protected:
-	bool _set(const StringName &p_name, const Variant &p_value);
-	bool _get(const StringName &p_name, Variant &r_ret) const;
-	void _default_property_list(List<PropertyInfo> *p_list) const;
-	void _get_property_list(List<PropertyInfo> *p_list) const;
-	void _set_default_property_values(Dictionary values);
-	static void _bind_methods();
-
-public:
-	Variant get_default_property_value() const;
-
-	void get_parameter_list(List<PropertyInfo> *r_list) const;
-
-	virtual String get_caption() const;
-	virtual float process(float p_time, bool p_seek);
-
-	AnimationNodeStateUpdate();
-};
-
 
 
 #endif // ANIMATION_NODE_FLASH_H
